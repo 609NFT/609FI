914ce63f1d1d156391a7c7c843831d9e52f8e8d8
609NFT <boisjolibrian@gmail.com>
2024-06-06 17:09:52 -0400

    latest working version

--- src/app/components/AppWalletProvider.tsx
+++ src/app/components/AppWalletProvider.tsx
@@ -7,7 +7,6 @@ import {
 } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { Connection } from "@solana/web3.js";
 
 // Default styles that can be overridden by your app
 require("@solana/wallet-adapter-react-ui/styles.css");
@@ -17,17 +16,16 @@ export default function AppWalletProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const network = WalletAdapterNetwork.Mainnet;
   const endpoint = useMemo(
     () =>
       "https://mainnet.helius-rpc.com/?api-key=22ef82dc-8230-48c5-b093-f2cc9acf690f",
-    [network]
+    []
   );
   const wallets = useMemo(
     () => [
       // Add any wallet adapters here
     ],
-    [network]
+    []
   );
 
   return (
